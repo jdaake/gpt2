32b2aba68eefc6eab7663a88c6f6a851a7250d9d
Jordan Daake <daakejl@gmail.com>
2023-04-29 23:55:34 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='gpt',
+    version='1.0.0',
+    description='OpenAI Completion API CLI Utility',
+    long_description=readme,
+    author='Jordan Daake',
+    author_email='daakejl@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['openai', 'python-dotenv'],
+    entry_points={
+        'console_scripts': 'gpt=gpt.cli:main',
+    }
+)
---
+++ src/gpt/cli.py
@@ -0,0 +1,28 @@
+import argparse
+import openai
+
+openai.api_key = ''
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(
+        prog='gpt', description='Question to ask GPT')
+    parser.add_argument('q', help='Enter question to ask GPT')
+    return parser.parse_args()
+
+
+def main():
+    args = arg_parse()
+    if args.q:
+        print(ask_question(args.q))
+    else:
+        return None
+
+
+def ask_question(question: str):
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"{question}",
+        max_tokens=200
+    )
+    return res['choices'][0]['text']
